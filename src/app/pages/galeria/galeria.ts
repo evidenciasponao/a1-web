b3721c61eed2a224068b5c910cbb6221c5598eb1
evidenciasponao <98203936+evidenciasponao@users.noreply.github.com>
2026-06-23 14:36:06 -0500

    Update prices of manillas to $20.000 COP

--- src/app/pages/galeria/galeria.ts
+++ src/app/pages/galeria/galeria.ts
@@ -12,19 +12,19 @@ export class Galeria {
   manillas = [
     {
       nombre: 'Manilla Negra',
-      precio: '$40.000 COP',
+      precio: '$20.000 COP',
       imagen: 'images/manilla1.jpg',
       descripcion: 'Diseño elegante.'
     },
     {
       nombre: 'Manilla Roja',
-      precio: '$50.000 COP',
+      precio: '$20.000 COP',
       imagen: 'images/manilla2.jpg',
       descripcion: 'Acabado premium.'
     },
     {
       nombre: 'Manilla Azul',
-      precio: '$60.000 COP',
+      precio: '$20.000 COP',
       imagen: 'images/manilla3.jpg',
       descripcion: 'Diseño exclusivo.'
     }
