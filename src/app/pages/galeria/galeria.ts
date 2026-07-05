c6873cd2b52e709578020bcd3a627c8b5ddf4d01
Anderson M <evidenciasponao@gmail.com>
2026-07-05 03:22:53 +0200

    update gallery api

--- src/app/pages/galeria/galeria.ts
+++ src/app/pages/galeria/galeria.ts
@@ -2,42 +2,78 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-galeria',
-  imports: [CommonModule],
-  templateUrl: './galeria.html',
-  styleUrl: './galeria.css'
+    selector: 'app-galeria',
+    imports: [CommonModule],
+    templateUrl: './galeria.html',
+    styleUrl: './galeria.css'
 })
 export class Galeria {
 
-  manillas = [
-    {
-      nombre: 'Manilla Negra',
-      precio: '$40.000 COP',
-      imagen: 'images/manilla1.jpg',
-      descripcion: 'Diseño elegante.'
-    },
-    {
-      nombre: 'Manilla Roja',
-      precio: '$50.000 COP',
-      imagen: 'images/manilla2.jpg',
-      descripcion: 'Acabado premium.'
-    },
-    {
-      nombre: 'Manilla Azul',
-      precio: '$60.000 COP',
-      imagen: 'images/manilla3.jpg',
-      descripcion: 'Diseño exclusivo.'
+    manillas = [
+        {
+            nombre: 'Manilla Negra',
+            precio: '$40.000 COP',
+            imagen: 'images/manilla1.jpg',
+            descripcion: 'Diseño elegante.'
+        },
+        {
+            nombre: 'Manilla Roja',
+            precio: '$50.000 COP',
+            imagen: 'images/manilla2.jpg',
+            descripcion: 'Acabado premium.'
+        },
+        {
+            nombre: 'Manilla Azul',
+            precio: '$60.000 COP',
+            imagen: 'images/manilla3.jpg',
+            descripcion: 'Diseño exclusivo.'
+        },
+        {
+            nombre: 'Manilla Colores',
+            precio: '$15.000 COP',
+            imagen: 'images/manilla4.jpg',
+            descripcion: 'Diseño elegante.'
+        },
+        {
+            nombre: 'Manilla Dorada',
+            precio: '$15.000 COP',
+            imagen: 'images/manilla5.jpg',
+            descripcion: 'Acabado premium.'
+        },
+        {
+            nombre: 'Manilla Corazon',
+            precio: '$60.000 COP',
+            imagen: 'images/manilla6.jpg',
+            descripcion: 'Diseño exclusivo.'
+        },
+        {
+            nombre: 'Manilla Corazon Blanca',
+            precio: '$40.000 COP',
+            imagen: 'images/manilla7.jpg',
+            descripcion: 'Diseño elegante.'
+        },
+        {
+            nombre: 'Manilla Corazon Negra',
+            precio: '$50.000 COP',
+            imagen: 'images/manilla8.jpg',
+            descripcion: 'Acabado premium.'
+        },
+        {
+            nombre: 'Manilla 1111',
+            precio: '$60.000 COP',
+            imagen: 'images/manilla9.jpg',
+            descripcion: 'Diseño exclusivo.'
+        }
+    ];
+
+    manillaSeleccionada: any = null;
+
+    abrirManilla(manilla: any) {
+        this.manillaSeleccionada = manilla;
     }
-  ];
 
-  manillaSeleccionada: any = null;
-
-  abrirManilla(manilla: any) {
-    this.manillaSeleccionada = manilla;
-  }
-
-  cerrarManilla() {
-    this.manillaSeleccionada = null;
-  }
+    cerrarManilla() {
+        this.manillaSeleccionada = null;
+    }
 
 }
